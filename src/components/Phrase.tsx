24279172fa50b95ca8b91e66a8b5e74a580d399c
alenmonti <amonti@fi.uba.ar>
2025-02-19 21:07:59 -0300

    translate no

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Phrase = () => {
   return (
-    <div className='h-fit bg-primary px-8 py-16'>
+    <div className='w-full bg-primary px-8 py-16'>
       <h1 className='text-white text-center text-2xl font-bold'>Cree en tu talento es tuyo el pincel</h1>
     </div>
   );
